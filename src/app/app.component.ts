1f3b0dbf40effc10a938c015cfd6fda1f1f78e08
wellwind <wellwind@mail2000.com.tw>
2017-02-11 13:36:36 +0800

    Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   inputHint = "What needs to be done?";
+
+  todoText;
+  todos: any[];
+
+  ngOnInit() {
+    this.todos = [];
+  }
+
+  addTodo(){
+    this.todos.push(this.todoText);
+    console.log(this.todos);
+  }
 }
