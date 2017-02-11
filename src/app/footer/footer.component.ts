f9d77c06d059f3ed056fe63ea2d06ad2a6afe15b
wellwind <wellwind@mail2000.com.tw>
2017-02-11 13:55:13 +0800

    @Input

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,18 @@
+import { TodoItem } from './../todo-item';
+import {Input, Component,  OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  @Input() todos: TodoItem[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
